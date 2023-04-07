abd38471f15c0f1045677a6627973aa98e3bcd12
Heljhum Enad <heljhumenad@gmail.com>
2023-04-07 09:15:23 +0800

    update: show non field error

--- grados/app/views/accounts.py
+++ grados/app/views/accounts.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import views
 from grados.app.accounts.models import CustomUser
@@ -8,5 +9,7 @@ from grados.app.accounts.models import CustomUser
 class AccountTemplateView(views.LoginView):
     template_name = "accounts/login.html"
 
-class DashboardTemplateView(LoginRequiredMixin,TemplateView):
+class DashboardTemplateView(LoginRequiredMixin, TemplateView):
+    login_url = reverse_lazy("login")
     template_name = "accounts/dashboard.html"
+    redirect_field_name = "next_link"
