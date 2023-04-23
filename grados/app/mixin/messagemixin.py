11eecb7f2d8908c2350a696ca1ba5c60cfa68110
Heljhum Enad <heljhumenad@gmail.com>
2023-04-23 15:40:46 +0800

    :recyle: update message framework

--- grados/app/mixin/messagemixin.py
+++ grados/app/mixin/messagemixin.py
@@ -6,13 +6,9 @@ from grados.app.mixin.loginrequired import LoginRequiredMixins
 
 class MessageMixins(LoginRequiredMixins, SuccessMessageMixin):
     success_message = _(
-        "%(field)s is sucessfully %(messages)s"
+        "Your data is sucessfully %(messages)s"
     )
 
-    @property
-    def field_name(self):
-        return NotImplemented
-    
     @property
     def messages(self):
         return NotImplemented
@@ -20,6 +16,5 @@ class MessageMixins(LoginRequiredMixins, SuccessMessageMixin):
     def get_success_message(self, cleaned_data, **kwargs):
         return self.success_message % dict(
             cleaned_data,
-            field = self.object.field_name,
             messages = self.messages
         )
