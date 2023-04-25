ce3e2c23884575bc261ec6542826b5f51390302a
Heljhum Enad <heljhumenad@gmail.com>
2023-04-25 23:29:13 +0800

    :construction: add factories for testing

---
+++ grados/app/tests/factories/teacher_factory.py
@@ -0,0 +1,29 @@
+# Teacher Factory
+from factory import SubFactory
+from factory.django import DjangoModelFactory
+from faker import Factory
+
+from grados.app.teachers.models import Teacher
+
+
+faker = Factory.creat()
+
+# TODO: Add some faker data for this class
+class TeacherFactory(DjangoModelFactory):
+    class Meta:
+        model = Teacher
+
+    birth_date = faker.date(
+        pattern = '%m/%d/%Y',
+        end_datetime = None,
+    )
+
+    designation_as = faker.random_elements(
+        elements = (
+            'Mrs.',
+            'Mr.',
+            'Miss',
+            'Dr.',
+            'Atty.'
+        )
+    )
