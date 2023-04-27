3c91c6b6466105443058a538fa30e86d3fbdd7f2
Heljhum Enad <heljhumenad@gmail.com>
2023-04-27 23:41:43 +0800

    :construction: create the models for the students

--- grados/app/students/models.py
+++ grados/app/students/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+from grados.app.core.models import TimeStampModel
+from grados.app.teachers.models import Teacher
+
+# TODO: Create normalize table for students and teacher
+class Student(model.Model, TimeStampModel):
+    pass
+
