dc1a89815385f106fb5dba4c6d6578b2776efe65
Heljhum Enad <heljhumenad@gmail.com>
2023-04-28 22:43:00 +0800

    :construction: add models for students

--- grados/app/students/models.py
+++ grados/app/students/models.py
@@ -3,8 +3,38 @@ from django.utils.translation import gettext as _
 
 from grados.app.core.models import TimeStampModel
 from grados.app.teachers.models import Teacher
+from grados.app.parents.models import Parent
 
 # TODO: Create normalize table for students and teacher
 class Student(model.Model, TimeStampModel):
-    pass
+    
+    teacher_id = models.ForeignKey(
+        Teacher,
+        on_delete = models.CASCADE,
+        verbose_name = _('Students')
+    )
+    
+    parent_id = models.ForeignKey(
+        Parent,
+        on_delete = models.CASCADE,
+        verbose_name = _('Parents')
+    )
+
+    age = models.SmallIntegerField(
+        max_length = 100,
+        verbose_name = _('Age'),
+        blank = False
+    )
+
+    
+            
+    class Meta:
+        db_table = _('students')
+        verbose_name = _('Student')
+        verbose_name_plural = _('Students')
+        ordering = ['id']
+
+    def __str__(self):
+        pass
+    
 
