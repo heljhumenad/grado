428b2f7772e5770df11e076428e9f42d32fcc00f
Heljhum Enad <heljhumenad@gmail.com>
2023-05-06 17:47:43 +0800

    :construction: add role model attribute

--- grados/app/accounts/models.py
+++ grados/app/accounts/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 from django.utils.translation import gettext as _
 
+from grados.app.core.models import TimeStampModel
+
 
 class CustomUserManager(UserManager):
     pass # check if whats the use of UserManager
@@ -16,3 +18,45 @@ class CustomUser(AbstractUser):
     @property
     def get_user_email(self):
         return self.email
+
+
+class Role(models.Model, TimeStampModel):
+    class Meta:
+        abstract = True
+
+
+    ROLE_NAME = (
+        ('Admin': 'Administrator'),
+        ('Parents': 'Parents'),
+        ('Teacher': 'Teacher'),
+        ('Editor': 'Editor'),
+        ('Approver': 'Approver'),
+
+    )
+
+    role_name = models.CharField(
+        max_length = 200,
+        choices = ROLE_NAME,
+        blank = False,
+        verbose_name = _('Role Name')
+    )
+
+    role_desc = models.CharField(
+        max_length = 200,
+        verbose_name = _('Role Description'),
+        blank = False,
+    )
+
+    role_mapping_id = models.CharField(
+        max_length = 200,
+        verbose_name = _('Role Mapping'),
+        blank = False
+    )
+
+    role_hash = models.CharField(
+        max_length = 200,
+        verbose_name = _('Role Hash'),
+        blank = False
+    )
+    
+
