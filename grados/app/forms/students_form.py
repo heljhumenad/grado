1c8877b1f9f904fe6c000d710af16882aeeefc58
Heljhum Enad <heljhumenad@gmail.com>
2023-05-08 13:22:42 +0800

    :add student form

--- grados/app/forms/students_form.py
+++ grados/app/forms/students_form.py
@@ -2,11 +2,26 @@ from django import forms
 from django.utils.translation import gettext as _
 
 from grados.app.students.models import Student
+from grados.app.teacher.models import Teacher
 
 
 # TODO: Add models for students and create normalize table from it
 class StudentsForm(forms.ModelForm):
+
+    teacher_id = forms.ModelChoiceField(
+        queryset = Teacher.objects.order_by("teacher_accounts_id"),
+        empty_label = _('Choose Teacher'),   
+    )
+
     class Meta:
         verbose_name = _('Students Form')
         verbose_plural_name = _('Teachers Forms')
         model = Student
+        fields = [
+            'teacher_id',
+            'first_name',
+            'last_name',
+            'middle_name',
+            'age'
+        ]
+        ordering = ['-id']
