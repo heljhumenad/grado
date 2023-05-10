593e458b6360194de148375ced97c20f19162f23
Heljhum Enad <heljhumenad@gmail.com>
2023-05-10 22:11:27 +0800

    add: view for the students

--- grados/app/views/students.py
+++ grados/app/views/students.py
@@ -1,7 +1,21 @@
+from django.utils.translation import gettext as _
+from djang.urls import reverse_lazy
 from django.views.generic import (
     TemplateView
 )
 
+from grados.app.students.models import Student
+from grados.app.mixin.messagemixin import MessageMixins
+from grados.app.forms.student_form import
+
 
 class StudentsTemplateView(TemplateView):
-    pass
+    template_name = 'students/index.html'
+
+# TODO: add the student urls
+class StudentCreateView(MessageMixins, CreateView):
+    template_name = 'students/student_add.html'
+    form_class = students_form.StudentForm
+    message = 'added'
+    success_url = reverse_lazy('student:student_add')
+
