39ac5602ba5df06807752d6ecf6bf5b779993286
Heljhum Enad <heljhumenad@gmail.com>
2023-05-29 23:06:21 +0800

    add: neccessary class for core

---
+++ grados/app/core/errors.py
@@ -0,0 +1,5 @@
+# All error that will happen in the class it will ineherit this class
+
+class GradoError:
+    def __init__(self):
+        pass
---
+++ grados/app/core/loggers.py
@@ -0,0 +1,5 @@
+# Logging API interface for the application
+
+class GradoLoggersInterface:
+    def __init__(self):
+        pass
